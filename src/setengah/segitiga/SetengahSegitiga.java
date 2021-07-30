66c41f6ff3fcafc7ab4739ec1ec225ab91e920e5
AriestaPurwa <85605545+AriestaPurwa@users.noreply.github.com>
2021-07-30 14:05:47 +0700

    Add files via upload

---
+++ src/setengah/segitiga/SetengahSegitiga.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package setengah.segitiga;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class SetengahSegitiga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b;
+        for (a = 1; a <= 4; a++){
+            System.out.println(" ");
+            
+            for (b = 1; b <= a; b++){
+            System.out.print("*");
+        }
+        }
+        
+        
+    }
+    
+}
